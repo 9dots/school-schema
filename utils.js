f686feb5defd69594ae01454a9129e054d7bea32
danleavitt0 <danleavitt0@gmail.com>
2018-04-12 10:26:29 -0700

    update difficulty and grade enums

--- utils.js
+++ utils.js
@@ -66,20 +66,20 @@ const date = { type: 'string', format: 'date-time' }
 const lesson = Schema('string').min(0)
 const uuid = Schema('string').min(10)
 const grade = Schema('string').enum([
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13'
 ])
 
 module.exports = {
