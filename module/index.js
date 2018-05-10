ce4f13ed4024a6df33d79a24e8ac1be26d6d73ea
danleavitt0 <danleavitt0@gmail.com>
2018-05-10 10:38:47 -0700

    fix exports

--- module/index.js
+++
@@ -1,6 +0,0 @@
-const validate = require('@weo-edu/validate')
-const Schema = require('@weo-edu/schema')
-// const { firebaseRef } = require('../utils')
-const Course = require('../course').default
-
-exports.default = Course
--- pages/modules.js
+++
@@ -1,6 +0,0 @@
-const firebase = require('firebase')
-require('firebase/firestore')
-
-const firestore = firebase.firestore()
-
-firestore.collection('modules').where('featured', '==', true)
